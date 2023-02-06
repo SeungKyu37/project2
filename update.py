1a47fde2f2dce291a684afd1221363917b752203
SeungKyu37 <xmcmrb@naver.com>
2023-02-06 11:38:05 +0900

    updated

--- update.py
+++ update.py
@@ -5,45 +5,68 @@ import numpy as np
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
-service_key = '4d42486779706d3034365957634870'
-data = []
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
-for j in range(1,2):
-    url = f'http://openapi.seoul.go.kr:8088/{service_key}/json/tbLnOpendataRentV/{1+((j-1)*1000)}/{j*1000}'
-    print(url)
-    req = requests.get(url)
-    content = req.json()
-    con = content['tbLnOpendataRentV']['row']
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.experimental_memo to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-    for h in con:
-        dic = {}
-        dic['SGG_CD'] = h['SGG_CD']
-        dic['SGG_NM'] = h['SGG_NM']
-        dic['BJDONG_CD'] = h['BJDONG_CD']
-        dic['BJDONG_NM'] = h['BJDONG_NM']
-        dic['BOBN'] = h['BOBN']
-        dic['BUBN'] = h['BUBN']
-        dic['FLR_NO'] = h['FLR_NO']
-        dic['CNTRCT_DE'] = h['CNTRCT_DE']
-        dic['RENT_GBN'] = h['RENT_GBN']
-        dic['RENT_AREA'] = h['RENT_AREA']
-        dic['RENT_GTN'] = h['RENT_GTN']
-        dic['RENT_FEE'] = h['RENT_FEE']
-        dic['BLDG_NM'] = h['BLDG_NM']
-        dic['BUILD_YEAR'] = h['BUILD_YEAR']
-        dic['HOUSE_GBN_NM'] = h['HOUSE_GBN_NM']
-        data.append(dic)
-#   ===
-# --
-df = pd.DataFrame(data)
-df['BOBN'].replace('', np.nan, inplace=True)
-df['BUBN'].replace('', np.nan, inplace=True)
-df['BLDG_NM'].replace('', np.nan, inplace=True)
-df['BUILD_YEAR'].replace('', np.nan, inplace=True)
-df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
-df['CNTRCT_DE'] = df['CNTRCT_DE'].apply(lambda x: pd.to_datetime(str(x), format="%Y/%m/%d"))
-df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
-df['CNTRCT_DE'].replace('T00:00:00', '', inplace=True)
-df = df.dropna()
-st.write(df)
-# df.to_csv('data.csv',encoding='euc-kr', index=False)
+rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['word'])
+
+# service_key = '4d42486779706d3034365957634870'
+# data = []
+
+# for j in range(1,2):
+#     url = f'http://openapi.seoul.go.kr:8088/{service_key}/json/tbLnOpendataRentV/{1+((j-1)*1000)}/{j*1000}'
+#     print(url)
+#     req = requests.get(url)
+#     content = req.json()
+#     con = content['tbLnOpendataRentV']['row']
+
+#     for h in con:
+#         dic = {}
+#         dic['SGG_CD'] = h['SGG_CD']
+#         dic['SGG_NM'] = h['SGG_NM']
+#         dic['BJDONG_CD'] = h['BJDONG_CD']
+#         dic['BJDONG_NM'] = h['BJDONG_NM']
+#         dic['BOBN'] = h['BOBN']
+#         dic['BUBN'] = h['BUBN']
+#         dic['FLR_NO'] = h['FLR_NO']
+#         dic['CNTRCT_DE'] = h['CNTRCT_DE']
+#         dic['RENT_GBN'] = h['RENT_GBN']
+#         dic['RENT_AREA'] = h['RENT_AREA']
+#         dic['RENT_GTN'] = h['RENT_GTN']
+#         dic['RENT_FEE'] = h['RENT_FEE']
+#         dic['BLDG_NM'] = h['BLDG_NM']
+#         dic['BUILD_YEAR'] = h['BUILD_YEAR']
+#         dic['HOUSE_GBN_NM'] = h['HOUSE_GBN_NM']
+#         data.append(dic)
+# #   ===
+# # --
+# df = pd.DataFrame(data)
+# df['BOBN'].replace('', np.nan, inplace=True)
+# df['BUBN'].replace('', np.nan, inplace=True)
+# df['BLDG_NM'].replace('', np.nan, inplace=True)
+# df['BUILD_YEAR'].replace('', np.nan, inplace=True)
+# df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
+# df['CNTRCT_DE'] = df['CNTRCT_DE'].apply(lambda x: pd.to_datetime(str(x), format="%Y/%m/%d"))
+# df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
+# df['CNTRCT_DE'].replace('T00:00:00', '', inplace=True)
+# df = df.dropna()
+# st.write(df)
+# # df.to_csv('data.csv',encoding='euc-kr', index=False)
