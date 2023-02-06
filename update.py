e1ea635d27b1b97a9055dd5952a23fcd2940d675
SeungKyu37 <xmcmrb@naver.com>
2023-02-06 17:39:06 +0900

    updated

--- update.py
+++ update.py
@@ -37,6 +37,7 @@ import sqlite3
 
 # dbConn.commit()
 
+## 출력
 #[2]DB연결 및 커서 객체 생성
 dbConn=sqlite3.connect("mydata.db")
 cs=dbConn.cursor()
@@ -48,53 +49,60 @@ def db_list():
     return sugg
 
 list = db_list()     
-df = pd.DataFrame(list, columns=['SGG_CD','SGG_NM','BJDONG_CD','BJDONG_NM','BOBN','BUBN','FLR_NO','CNTRCT_DE','RENT_GBN','RENT_AREA','RENT_GTN','RENT_FEE','BLDG_NM','BUILD_YEAR','HOUSE_GBN_NM'])     
-df = df.drop(0, axis=0)
-st.dataframe(df)
+df0 = pd.DataFrame(list, columns=['SGG_CD','SGG_NM','BJDONG_CD','BJDONG_NM','BOBN','BUBN','FLR_NO','CNTRCT_DE','RENT_GBN','RENT_AREA','RENT_GTN','RENT_FEE','BLDG_NM','BUILD_YEAR','HOUSE_GBN_NM'])     
+df0 = df0.drop(0, axis=0)
+st.write(df0)
+
+
+service_key = '4d42486779706d3034365957634870'
+data = []
+
+for j in range(1,4):
+    url = f'http://openapi.seoul.go.kr:8088/{service_key}/json/tbLnOpendataRentV/{1+((j-1)*1000)}/{j*1000}'
+    print(url)
+    req = requests.get(url)
+    content = req.json()
+    con = content['tbLnOpendataRentV']['row']
+
+    for h in con:
+        dic = {}
+        dic['SGG_CD'] = h['SGG_CD']
+        dic['SGG_NM'] = h['SGG_NM']
+        dic['BJDONG_CD'] = h['BJDONG_CD']
+        dic['BJDONG_NM'] = h['BJDONG_NM']
+        dic['BOBN'] = h['BOBN']
+        dic['BUBN'] = h['BUBN']
+        dic['FLR_NO'] = h['FLR_NO']
+        dic['CNTRCT_DE'] = h['CNTRCT_DE']
+        dic['RENT_GBN'] = h['RENT_GBN']
+        dic['RENT_AREA'] = h['RENT_AREA']
+        dic['RENT_GTN'] = h['RENT_GTN']
+        dic['RENT_FEE'] = h['RENT_FEE']
+        dic['BLDG_NM'] = h['BLDG_NM']
+        dic['BUILD_YEAR'] = h['BUILD_YEAR']
+        dic['HOUSE_GBN_NM'] = h['HOUSE_GBN_NM']
+        data.append(dic)
+#   ===
+# --
+df = pd.DataFrame(data)
+df['BOBN'].replace('', np.nan, inplace=True)
+df['BUBN'].replace('', np.nan, inplace=True)
+df['BLDG_NM'].replace('', np.nan, inplace=True)
+df['BUILD_YEAR'].replace('', np.nan, inplace=True)
+df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
+df['CNTRCT_DE'] = df['CNTRCT_DE'].apply(lambda x: pd.to_datetime(str(x), format="%Y/%m/%d"))
+df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
+df['CNTRCT_DE'].replace('T00:00:00', '', inplace=True)
+df = df.dropna()
+
+# df.to_csv('data.csv',encoding='euc-kr', index=False)
+
+df.drop(index = df[df['CNTRCT_DE']=='2023-01-30'].index, inplace=True)
+
+df2 = pd.concat([df,df0])
+st.write(df2)
+
+df2.to_sql('budonsan',dbConn)
 
 cs.close()
 dbConn.close()
-
-# service_key = '4d42486779706d3034365957634870'
-# data = []
-
-# for j in range(1,2):
-#     url = f'http://openapi.seoul.go.kr:8088/{service_key}/json/tbLnOpendataRentV/{1+((j-1)*1000)}/{j*1000}'
-#     print(url)
-#     req = requests.get(url)
-#     content = req.json()
-#     con = content['tbLnOpendataRentV']['row']
-
-#     for h in con:
-#         dic = {}
-#         dic['SGG_CD'] = h['SGG_CD']
-#         dic['SGG_NM'] = h['SGG_NM']
-#         dic['BJDONG_CD'] = h['BJDONG_CD']
-#         dic['BJDONG_NM'] = h['BJDONG_NM']
-#         dic['BOBN'] = h['BOBN']
-#         dic['BUBN'] = h['BUBN']
-#         dic['FLR_NO'] = h['FLR_NO']
-#         dic['CNTRCT_DE'] = h['CNTRCT_DE']
-#         dic['RENT_GBN'] = h['RENT_GBN']
-#         dic['RENT_AREA'] = h['RENT_AREA']
-#         dic['RENT_GTN'] = h['RENT_GTN']
-#         dic['RENT_FEE'] = h['RENT_FEE']
-#         dic['BLDG_NM'] = h['BLDG_NM']
-#         dic['BUILD_YEAR'] = h['BUILD_YEAR']
-#         dic['HOUSE_GBN_NM'] = h['HOUSE_GBN_NM']
-#         data.append(dic)
-# #   ===
-# # --
-# df = pd.DataFrame(data)
-# df['BOBN'].replace('', np.nan, inplace=True)
-# df['BUBN'].replace('', np.nan, inplace=True)
-# df['BLDG_NM'].replace('', np.nan, inplace=True)
-# df['BUILD_YEAR'].replace('', np.nan, inplace=True)
-# df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
-# df['CNTRCT_DE'] = df['CNTRCT_DE'].apply(lambda x: pd.to_datetime(str(x), format="%Y/%m/%d"))
-# df['CNTRCT_DE'] = df['CNTRCT_DE'].astype('str')
-# df['CNTRCT_DE'].replace('T00:00:00', '', inplace=True)
-# df = df.dropna()
-# st.write(df)
-# # df.to_csv('data.csv',encoding='euc-kr', index=False)
-
