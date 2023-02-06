5ad02235ba4058d58fb12ea3064089081cdacdc0
SeungKyu37 <xmcmrb@naver.com>
2023-02-06 16:25:00 +0900

    updated

--- predict.py
+++ predict.py
@@ -7,7 +7,6 @@ matplotlib.use('Agg')
 import plotly.graph_objects as go
 import geopandas as gp
 import json
-
 def run_predict():
     st.title('전세 예측')
     df = pd.read_csv('data/bds_data.csv', encoding='cp949')
@@ -30,62 +29,19 @@ def run_predict():
             fig.add_scatter(x=df2['CNTRCT_DE'], y=df2['RENT_GTN'], name=dong)
     fig.update_layout(xaxis_title='날짜', yaxis_title='보증금(k=천만원)')
     st.plotly_chart(fig)
-    m_df = pd.read_csv('data/bds_data.csv', encoding='euc-kr')
-    m_gu = pd.read_csv('data/gu_j_d_mean.csv', encoding='euc-kr')
-    geo = gp.read_file('data/layer1.json')
-    st.header("지역구별 평균 실거래가 확인")
-    with open('data/layer1.json', encoding='UTF-8') as f:
-        data = json.load(f)
-    for x in data['features']:
-        x['id'] = x['properties']['SIG_KOR_NM']
-    for idx, _ in enumerate(data['features']):
-        print(data['features'][idx]['id'])
-    mapper = [
-    ('송파구', '송파구'),
-    ('강남구', '강남구'),
-    ('성동구', '성동구'),
-    ('구로구', '구로구'),
-    ('영등포구', '영등포구'),
-    ('양천구', '양천구'),
-    ('도봉구', '도봉구'),
-    ('서초구', '서초구'),
-    ('관악구', '관악구'),
-    ('중구', '중구'),
-    ('동대문구', '동대문구'),
-    ('광진구', '광진구'),
-    ('은평구', '은평구'),
-    ('중랑구', '중랑구'),
-    ('노원구', '노원구'),
-    ('강동구', '강동구'),
-    ('동작구', '동작구'),
-    ('마포구', '마포구'),
-    ('강북구', '강북구'),
-    ('강서구', '강서구'),
-    ('용산구', '용산구'),
-    ('성북구', '성북구'),
-    ('금천구', '금천구'),
-    ('종로구', '종로구'),
-    ('서대문구', '서대문구'),
-    ]
-    get_region = lambda SGG_NM: [x[1] for x in mapper if x[0] == SGG_NM][0]
-    m_gu['geo_region'] = m_gu.SGG_NM.apply(get_region)
-    cal = st.date_input('날짜를 선택하세요')
-    sel=m_gu[m_gu['CNTRCT_DE']== f'{cal}']
-    fig = px.choropleth_mapbox(
-        sel,
-        geojson=data,
-        locations='geo_region',
-        color='RENT_GTN',
-        color_continuous_scale=["orange", "red",
-                                         "green", "blue",
-                                         "purple"],
-        # featureidkey="properties.CTP_KOR_NM", # featureidkey를 사용하여 id 값을 갖는 키값 지정
-        mapbox_style="carto-positron",
-        zoom=10,
-        center = {"lat": 37.517, "lon": 127.047},
-        opacity=0.6,
-        labels={'RENT_GTN':'가격'}
-    )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    # fig.show()
+    ef = "data/ef.geojson"
+    dgg = gp.read_file(ef,encoding='euc-kr')
+    #map_df = gp.read_file(fp)
+    #map_df.to_crs(pyproj.CRS.from_epsg(4326), inplace=True)
+    ab = "data/dong_j_d_mean.csv"
+    dff =  pd.read_csv(ab,encoding='euc-kr')
+    date1 = st.date_input("날짜선택")
+    date2 = st.selectbox("동선택", dgg['adm_nm'].unique())
+    map_dong = dgg[dgg['adm_nm'] == f'{date2}']
+    map_si = dff[dff['CNTRCT_DE'] == f'{date1}']
+    merged = map_dong.set_index('adm_nm').join(map_si.set_index('BJDONG_NM'))
+    fig = px.choropleth_mapbox(merged, geojson=merged.geometry, locations=merged.index, color="RENT_GTN", mapbox_style="carto-positron", zoom=9.8,
+    center = {"lat": 37.575651, "lon": 126.97689}, opacity=0.6)
+    fig.update_geos(fitbounds="locations", visible=True)
+    #fig.show()
     st.plotly_chart(fig)
--- update.py
+++ update.py
@@ -2,31 +2,58 @@ import requests
 import pandas as pd
 import streamlit as st
 import numpy as np
-from google.oauth2 import service_account
-from google.cloud import bigquery
+import csv
+import sqlite3
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
+# #[1]DB생성-->Conncetion객체의-->connect()함수를 사용하여 생성
+# dbConn=sqlite3.connect('mydata.db')
+# print(type(dbConn)) #sqlite3.Conncetion 객체.
 
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.experimental_memo to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
+# #[2]DB커서 객체 생성-->Conncetion객체인 dbConn을 사용해 Cursor객체를 생성.
+# cs=dbConn.cursor()
+# print(type(cs)) #sqlite3.Cursor객체.
 
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+# #[3]DB테이블 생성
+# # cs.execute("Create table if not exists tbl_PublicAPI)_")
+# cs.execute('Create table if not exists tbl_PublicAPI(SGG_CD varchar, SGG_NM varchar, BJDONG_CD varchar, BJDONG_NM varchar, BOBN varchar, BUBN varchar, FLR_NO varchar, CNTRCT_DE varchar, RENT_GBN varchar, RENT_AREA varchar, RENT_GTN varchar, RENT_FEE varchar, BLDG_NM varchar, BUILD_YEAR varchar, HOUSE_GBN_NM varchar)')
 
-# Print results.
-st.write("Some wise words from Shakespeare:")
-for row in rows:
-    st.write("✍️ " + row['word'])
+# #[1]csv파일 읽기-->open()사용-->csv.reader()메서드 사용하여 한 줄씩 읽기.
+# fileName="data/bds_data.csv"
+# file=open(fileName,"r")
+# reader=csv.reader(file)
+
+
+
+# #[3]순회하면서 할 일 처리.
+# arr=[]
+
+# for row in reader:
+#     # print(row)
+#     arr.append(row)
+    
+# for row in arr:
+#     strSQL="INSERT INTO tbl_PublicAPI(SGG_CD,SGG_NM,BJDONG_CD,BJDONG_NM,BOBN,BUBN,FLR_NO,CNTRCT_DE,RENT_GBN,RENT_AREA,RENT_GTN,RENT_FEE,BLDG_NM,BUILD_YEAR,HOUSE_GBN_NM)values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+#     cs.execute(strSQL,(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14]))
+
+# dbConn.commit()
+
+#[2]DB연결 및 커서 객체 생성
+dbConn=sqlite3.connect("mydata.db")
+cs=dbConn.cursor()
+
+#[4]DB데이터 출력
+def db_list():
+    cs.execute('SELECT * FROM tbl_PublicAPI')
+    sugg = cs.fetchall()
+    return sugg
+
+list = db_list()     
+df = pd.DataFrame(list, columns=['SGG_CD','SGG_NM','BJDONG_CD','BJDONG_NM','BOBN','BUBN','FLR_NO','CNTRCT_DE','RENT_GBN','RENT_AREA','RENT_GTN','RENT_FEE','BLDG_NM','BUILD_YEAR','HOUSE_GBN_NM'])     
+df = df.drop(0, axis=0)
+st.dataframe(df)
+
+cs.close()
+dbConn.close()
 
 # service_key = '4d42486779706d3034365957634870'
 # data = []
@@ -70,3 +97,4 @@ for row in rows:
 # df = df.dropna()
 # st.write(df)
 # # df.to_csv('data.csv',encoding='euc-kr', index=False)
+
