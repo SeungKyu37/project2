5cb096abc7f4c8c7595423309e531b927189423c
SeungKyu37 <xmcmrb@naver.com>
2023-02-06 14:32:31 +0900

    updated

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 
 import tensorflow as tf 
 import streamlit as st 
-from google.oauth2 import service_account
-from google.cloud import bigquery
 
 st.write(tf.__version__)
+
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import pandas as pd 
+
+st.write(pd.__version__)
+st.write("Done?")
