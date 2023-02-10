ca03de88da642840847edb53e6c4413a6ae15aba
SeungKyu37 <xmcmrb@naver.com>
2023-02-10 11:35:07 +0900

    updated

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ from chatbot import chatrun
 
 selected3 = option_menu(None, ["🏠Home", "🔎전월세 검색",  "📊전세 예측", '챗봇', '💬건의사항'], 
     # icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal",
+    default_index=0, orientation="horizontal",
     styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "gray", "font-size": "15px"}, 
+        
         "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#47C83E"},
     }
--- submit.bat
+++ submit.bat
@@ -1,4 +1,5 @@
 set root=C:\Users\hh\Desktop\project2
+call C:\Users\hh\Desktop\project2\venv\Scripts\activate
 call git add .
 call git commit -m "updated from batch file"
 call git push
