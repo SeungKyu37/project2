b9169af837e62fbbce186852487daf42e1ab334a
SeungKyu37 <xmcmrb@naver.com>
2023-02-10 11:39:32 +0900

    updated

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ selected3 = option_menu(None, ["🏠Home", "🔎전월세 검색",  "📊전세 
     default_index=0, orientation="horizontal",
     styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
-        
+        "icon": {"color": "gray", "font-size": "15px"}, 
         "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#47C83E"},
     }
