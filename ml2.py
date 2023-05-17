97bf1402f09ed948a1cb4f1b504e23aee67f9ece
SeungKyu37 <xmcmrb@naver.com>
2023-05-17 15:54:07 +0900

    230514

--- ml2.py
+++ ml2.py
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")
 import matplotlib.font_manager as fonm
 import prophet
 # import yfinance as yf
-from prophet import Prophet
+# from prophet import Prophet
 import matplotlib.dates as mdates
 from matplotlib import cm
 from tensorflow import keras
